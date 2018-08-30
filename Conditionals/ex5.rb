7f38421ab8876f82f21d45e6b9052985a72b9ff8
norskbrek <norskbrek@github.com>
2018-08-30 16:56:48 -0500

    Conditionals exercises

---
+++ Conditionals/ex5.rb
@@ -0,0 +1,15 @@
+number = 7
+
+if number
+  puts "My favorite number is #{number}."
+else
+  puts "I don't have a favorite number."
+end
+
+=begin
+
+"My favorite number is 7."
+
+Why? "number" evaluates to true.
+
+=end
